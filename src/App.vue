f3ba3a2858c1ba89166786d9215e600f82a58e9a
helloelio <fredwantblossom@gmail.com>
2021-12-27 17:59:37 +0300

    feat: Add game logic, Add backlight for buttons, Add change difference in game, add lost and win mes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="container">
     <div class="game">
-      <div class="game-element top-left"></div>
-      <div class="game-element top-right"></div>
-      <div class="game-element bottom-left"></div>
-      <div class="game-element bottom-right"></div>
+      <div
+        ref="one"
+        class="game-element top-left"
+        @click="handlerGame(1)"
+      ></div>
+      <div
+        ref="two"
+        class="game-element top-right"
+        @click="handlerGame(2)"
+      ></div>
+      <div
+        ref="three"
+        class="game-element bottom-left"
+        @click="handlerGame(3)"
+      ></div>
+      <div
+        ref="four"
+        class="game-element bottom-right"
+        @click="handlerGame(4)"
+      ></div>
     </div>
     <div class="controls">
-      <form action="">
-        <input type="radio" name="diff" value="easy" v-model="diff" />
-        easy
+      <form>
+        <input type="radio" name="diff" value="easy" v-model="diff" />Easy
         <input type="radio" name="diff" value="normal" v-model="diff" />
-        normal
+        Normal
         <input type="radio" name="diff" value="hard" v-model="diff" />
-        hard
+        Hard
       </form>
-      <button>Start Game</button>
-      <div>Round: {{ round }}</div>
+      <button class="btn" @click="startGame">Start Game</button>
+      <div class="round-counter">Round: {{ round }}</div>
+      <div class="winner" v-if="this.win">Congratulations your winner!</div>
+      <div class="lost" v-if="this.lost">You lost</div>
     </div>
   </div>
 </template>
@@ -26,13 +43,130 @@ export default {
   name: 'App',
   data() {
     return {
+      gameStarted: false,
       round: 1,
       diff: 'easy',
+      order: [],
+      playerOrder: [],
+      flash: 0,
+      intervalId: 0,
+      turn: 1,
+      good: true,
+      compTurn: false,
+      win: false,
+      lost: false,
     };
   },
+
+  computed: {
+    diffInterval() {
+      return this.diff === 'easy'
+        ? 1500
+        : this.diff === 'normal'
+        ? 1000
+        : this.diff === 'hard'
+        ? 400
+        : 0;
+    },
+  },
+
   methods: {
     getDiff(event) {
       this.diff = event;
+      this.gameStarted = false;
+      this.round = 1;
+    },
+    startGame() {
+      this.gameStarted = true;
+      this.round = 1;
+      this.win = false;
+      this.lost = false;
+      this.order = [];
+      this.playerOrder = [];
+      this.flash = 0;
+      this.intervalId = 0;
+      this.turn = 1;
+      this.good = true;
+      for (let i = 0; i < 20; i += 1) {
+        this.order.push(Math.floor(Math.random() * 4) + 1);
+      }
+      this.compTurn = true;
+      this.intervalId = setInterval(this.gameTurn, this.diffInterval);
+    },
+
+    gameTurn() {
+      if (this.flash == this.turn) {
+        clearInterval(this.intervalId);
+        this.compTurn = false;
+      }
+
+      if (this.compTurn) {
+        setTimeout(() => {
+          if (this.order[this.flash] == 1) this.lightingButton(1);
+          if (this.order[this.flash] == 2) this.lightingButton(2);
+          if (this.order[this.flash] == 3) this.lightingButton(3);
+          if (this.order[this.flash] == 4) this.lightingButton(4);
+          this.flash += 1;
+        }, 200);
+      }
+    },
+
+    lightingButton(index) {
+      switch (index) {
+        case 1:
+          this.lightingButtonHelper(this.$refs.one);
+          break;
+        case 2:
+          this.lightingButtonHelper(this.$refs.two);
+          break;
+        case 3:
+          this.lightingButtonHelper(this.$refs.three);
+          break;
+        case 4:
+          this.lightingButtonHelper(this.$refs.four);
+          break;
+      }
+    },
+
+    lightingButtonHelper(item) {
+      item.classList.add('light');
+      if (!this.win) {
+        setTimeout(() => {
+          item.classList.remove('light');
+        }, 400);
+      }
+    },
+
+    handlerGame(event) {
+      if (this.gameStarted) {
+        this.playerOrder.push(event);
+        this.check();
+      }
+    },
+
+    check() {
+      if (
+        this.playerOrder[this.playerOrder.length - 1] !==
+        this.order[this.playerOrder.length - 1]
+      ) {
+        this.good = false;
+        this.lost = true;
+      }
+      if (this.playerOrder.length == 5 && this.good) {
+        this.winGame();
+      }
+      if (this.turn == this.playerOrder.length && this.good && !this.win) {
+        this.turn += 1;
+        this.round += 1;
+        this.playerOrder = [];
+        this.compTurn = true;
+        this.flash = 0;
+        this.intervalId = setInterval(this.gameTurn, this.diffInterval);
+      }
+    },
+    winGame() {
+      this.gameStarted = false;
+      this.win = true;
     },
   },
 };
@@ -44,8 +178,29 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  font-size: 1.5rem;
+}
+.btn {
+  background: rgb(238, 174, 202);
+  background: radial-gradient(
+    circle,
+    rgba(238, 174, 202, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+  color: white;
+  text-shadow: 1px 1px black;
+  font-size: 1.5rem;
+  padding: 10px 40px;
+  margin: 10px 0;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+}
+.btn:hover {
+  transform: scale(1.01);
+  box-shadow: 0px 0px 10px 0px black;
 }
 .game {
   display: grid;
@@ -68,6 +223,9 @@ export default {
   border-top-left-radius: 100%;
   background-color: green;
 }
+.light {
+  opacity: 0.7;
+}
 .top-right {
   border-top-right-radius: 100%;
   background-color: red;
@@ -79,5 +237,18 @@ export default {
 .bottom-right {
   border-bottom-right-radius: 100%;
   background-color: blue;
+}
+
+.round-counter {
+  padding: 10px;
+  background-color: rgb(68, 19, 19);
+}
+.winner {
+  padding: 10px;
+  background-color: green;
+}
+.lost {
+  padding: 10px;
+  background-color: red;
 }
 </style>
