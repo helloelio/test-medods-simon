545f025e7670bf3be6b034a79022fb41c3e94ec9
helloelio <fredwantblossom@gmail.com>
2021-12-27 12:30:48 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="game">
+      <div class="game-element top-left"></div>
+      <div class="game-element top-right"></div>
+      <div class="game-element bottom-left"></div>
+      <div class="game-element bottom-right"></div>
+    </div>
+    <div class="controls">
+      <form action="">
+        <input type="radio" name="diff" value="easy" v-model="diff" />
+        easy
+        <input type="radio" name="diff" value="normal" v-model="diff" />
+        normal
+        <input type="radio" name="diff" value="hard" v-model="diff" />
+        hard
+      </form>
+      <button>Start Game</button>
+      <div>Round: {{ round }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data() {
+    return {
+      round: 1,
+      diff: 'easy',
+    };
+  },
+  methods: {
+    getDiff(event) {
+      this.diff = event;
+    },
   },
 };
 </script>
@@ -22,5 +46,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.game {
+  display: grid;
+  grid-template-columns: auto auto;
+  max-width: 500px;
+  margin: 0 auto 20px;
+}
+.controls {
+  max-width: 500px;
+  background-color: gray;
+  color: white;
+  margin: 0 auto;
+}
+.game-element {
+  height: 200px;
+  border: 10px solid #000;
+  cursor: pointer;
+}
+.top-left {
+  border-top-left-radius: 100%;
+  background-color: green;
+}
+.top-right {
+  border-top-right-radius: 100%;
+  background-color: red;
+}
+.bottom-left {
+  border-bottom-left-radius: 100%;
+  background-color: yellow;
+}
+.bottom-right {
+  border-bottom-right-radius: 100%;
+  background-color: blue;
 }
 </style>
